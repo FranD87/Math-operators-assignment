6d53fc62eaf96ad6a750b77d86792717e615ea5e
FranD87 <fdunne007@gmail.com>
2023-01-03 14:21:10 +0100

    completed

---
+++ Math_assign.py
@@ -0,0 +1,113 @@
+# Task one
+
+import math
+
+
+# a = 34
+#
+# b = 2
+#
+# print(a + b)
+#
+# print(a - b)
+#
+# print(a * b)
+#
+# print(a / b)
+#
+# print(a % b)
+#
+# print(a ** b)
+#
+# print(a // b)
+#
+# c = True
+#
+# d = False
+#
+# print(c * d)
+#
+# 5 * (2 - 3j) = (10 - 15j)
+#
+# second calculations
+#
+# e = 84
+#
+# f = 2.2
+#
+# print(a + b)
+#
+# print(a - b)
+#
+# print(a * b)
+#
+# print(a / b)
+#
+# print(a % b)
+#
+# print(a ** b)
+#
+# print(a // b)
+#
+# c = True
+#
+# d = False
+#
+# print(c * d)
+#
+# 5 * (6 - 34j) = (10 - 15j)
+
+
+# Task two
+
+# a = max(22, 3, 0.85)
+#
+# print('max(22, 3, 0.85) = ', a)
+
+# a = max(45, 6, 0.67)
+#
+# print('max(45, 6, 0.67) = ', a)
+
+# a = min(65, 76, 1.35)
+#
+# print('min(65, 76, 1.35) = ', a)
+
+# a = abs(-65.78)
+#
+# print('abs(-65.78) = ', a)
+
+# a = abs(-73.13)
+#
+# print('abs(-73.13) = ', a)
+
+# a = pow(5, 7)
+#
+# print('pow(5, 7) =', a)
+
+# a = pow(3,54)
+#
+# print('pow(3,54) =', a)
+
+# a = math.ceil(3/4)
+#
+# print('ceil(3/4) =', a)
+
+# a = math.floor(45/6)
+#
+# print('floor(45/6) =', a)
+
+# a = math.floor(32/4)
+#
+# print('floor(32/4) =', a)
+
+# a = max(True, False)
+#
+# print('max(True, False) =', a)
+
+# a = abs(4 - 3j)
+#
+# print('max(True, False) =', a)
+
+# a = abs(5 - 4g)
+#
+# print('abs(5 - 4g) =', a)
